43f11279a9a40da3c4df0cad4d6a9b94e0084496
Ed Fialkowski <edfialk@gmail.com>
2020-01-14 10:13:04 -0800

    skeleton style

--- components/Channels.vue
+++ components/Channels.vue
@@ -1,5 +1,133 @@
 <template>
   <div class="channels">
-    Channels
+    <div class="flex-spaced">
+      <div class="channels__title">
+        <span class="channels__title__text">Nomad List</span>
+        <span class="channels__title__icon"><font-awesome-icon :icon="['fas', 'chevron-down']" /></span>
+      </div>
+      <font-awesome-icon :icon="['fas', 'cog']" />
+    </div>
+    <div>
+      <font-awesome-icon :icon="['fas', 'comment-dots']" style="margin-right: 10px;" /> All threads
+    </div>
+    <div class="channels-block">
+      <div class="channels-block__title flex-spaced">
+        <div>CHANNELS</div>
+        <div>11</div>
+      </div>
+      <ul class="channels-block__items">
+        <li class="active">#general</li>
+        <li>#support</li>
+        <li>#marketing</li>
+        <li>#thailand</li>
+        <li>#bali</li>
+        <li>#poland</li>
+        <li>#australia</li>
+        <li>#jobs</li>
+        <li>#startups</li>
+        <li>#italy</li>
+        <li>#freelance</li>
+      </ul>
+    </div>
+    <div class="channels-block">
+      <div class="channels-block__title flex-spaced">
+        <div>FRIENDS</div>
+        <div>82</div>
+      </div>
+      <ul class="channels-block__items friends__list">
+        <li>
+          <div class="friend__icon friend__icon--active" />
+          <img src="https://i.pravatar.cc/32?img=68">
+          <span>Orlando Diggs</span>
+        </li>
+        <li>
+          <div class="friend__icon friend__icon--active" />
+          <img src="https://i.pravatar.cc/32?img=47">
+          <span>Carmen Velasco</span>
+        </li>
+        <li>
+          <div class="friend__icon" />
+          <img src="https://i.pravatar.cc/32?img=49">
+          <span>Marie Jensen</span>
+        </li>
+        <li>
+          <div class="friend__icon" />
+          <img src="https://i.pravatar.cc/32?img=33">
+          <span>Alex Lee</span>
+        </li>
+        <li>
+          <div class="friend__icon" />
+          <img src="https://i.pravatar.cc/32?img=18">
+          <span>Leo Gill</span>
+        </li>
+        <li>
+          <div class="friend__icon" />
+          <img src="https://i.pravatar.cc/32?img=9">
+          <span>Britney Cooper</span>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="scss">
+
+.channels {
+  padding: 35px 30px;
+  color: #B5B5B5;
+  & > div {
+    @apply mb-6;
+  }
+}
+.channels__title {
+  .channels__title__text {
+    font-size: 20px;
+    font-weight: 700;
+    @apply text-white;
+  }
+  .channels__title__icon {
+    font-size: 14px;
+  }
+}
+.channels-block {
+  font-size: 14px;
+}
+.channels-block__title {
+  font-size: 12px;
+  @apply mb-3;
+}
+.channels-block__items {
+  line-height: 17px;
+  li {
+    @apply p-2;
+  }
+
+  & .active {
+    font-weight: 700;
+    @apply text-white rounded;
+    background-color: rgba(255, 255, 255, 0.2);
+  }
+}
+.friends__list {
+  div {
+    @apply inline-block;
+  }
+  img {
+    @apply rounded inline-block ml-1 mr-3;
+  }
+}
+.friend__icon {
+  width: 8px;
+  height: 8px;
+  border-radius: 50%;
+  background-color: #666;
+}
+.friend__icon--active {
+  background-color: #70CC16;
+}
+</style>
--- components/Chat.vue
+++ components/Chat.vue
@@ -1,12 +1,85 @@
 <template>
   <div class="chatroom">
-    <header class="chatroom--header">
-      <div class="title">
+    <header class="chatroom__header">
+      <div class="chatroom__title">
         #general
+        <font-awesome-icon :icon="['far', 'star']" />
       </div>
-      <div>
-        users, search, bell, hamburger
+      <div class="chatroom__header--right">
+        <font-awesome-icon :icon="['far', 'user']" class="icon" />
+        <span class="icon">1,093</span>
+        <div class="search">
+          <input type="text" placeholder="Search..">
+          <font-awesome-icon :icon="['fas', 'search']" />
+        </div>
+        <font-awesome-icon :icon="['far', 'bell']" class="has-alert icon" />
+        <font-awesome-icon :icon="['fas', 'ellipsis-v']" class="icon" />
       </div>
     </header>
+    <ChatRoom />
+    <div class="chatroom__footer">
+      <div class="icon"><font-awesome-icon :icon="['fas', 'paperclip']" /></div>
+      <div class="icon"><font-awesome-icon :icon="['fas', 'microphone']" /></div>
+      <input class="chatbox" type="text" placeholder="Message in #general">
+      <div class="icon"><font-awesome-icon :icon="['far', 'smile']" /></div>
+    </div>
   </div>
 </template>
+
+<script>
+import ChatRoom from './ChatRoom'
+
+export default {
+
+  components: { ChatRoom }
+
+}
+</script>
+
+<style lang="scss">
+
+.chatroom {
+    background: #fff;
+    flex: 1;
+    height: 100vh;
+    @apply flex flex-col leading-loose;
+}
+.chatroom__header {
+  @apply flex justify-between p-4;
+  // border-bottom: 1px solid rgba(0,0,0,0.12);
+}
+.chatroom__title {
+  @apply font-bold text-xl;
+  // flex: 1;
+}
+.chatroom__footer {
+  @apply px-5 flex items-center text-lg;
+  // font-size: 14px;
+}
+.icon {
+  position: relative;
+  color: #8D8D8D;
+  @apply mx-3;
+  &:hover {
+    cursor: pointer;
+  }
+}
+.chatbox {
+  flex: 1;
+  @apply px-3 py-4;
+  &:focus {
+    @apply outline-none;
+  }
+}
+.search {
+  @apply border border-gray-500 rounded px-3 mx-3;
+  color: #8D8D8D;
+  input:focus {
+    @apply outline-none;
+  }
+}
+.chatroom__header--right {
+  @apply flex items-center;
+}
+
+</style>
