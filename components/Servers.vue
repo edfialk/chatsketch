389fefa8959af6f6f5f3323e0e610b20398eaf17
Ed Fialkowski <edfialk@gmail.com>
2020-01-30 10:27:25 -0800

    server seeds

--- components/Servers.vue
+++ components/Servers.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="sidebar">
     <img src="../assets/images/dots-os.png">
-    <div class="server-list">
+    <div class="server__list">
       <div v-for="(server, name) in servers" :key="name">
-        <img :src="getImage(server)" :alt="name">
+        <img :src="getImage(server)" :alt="name" :class="{ active: name === active }">
       </div>
     </div>
     <font-awesome-icon :icon="['fas', 'plus-circle']" color="#454444" style="font-size: 40px;" />
@@ -43,3 +43,28 @@ export default {
 
 }
 </script>
+
+<style lang="scss">
+.sidebar {
+    background: rgba(0,0,0,0.7);
+    height: 100vh;
+    width: 75px;
+    @apply p-3 flex flex-col items-center;
+}
+.server__list {
+    @apply flex flex-col items-center py-5 flex-grow;
+}
+.server__list div {
+  @apply py-2;
+}
+.server__list img {
+  opacity: 0.5;
+  cursor: pointer;
+}
+.server__list img:hover {
+  opacity: 0.8;
+}
+.server__list img.active {
+  opacity: 1;
+}
+</style>
